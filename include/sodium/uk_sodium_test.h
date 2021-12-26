ac9e7db5117e50667fe43e75bf67f9dd4212b7c3
Michalis Pappas <Michalis.Pappas@opensynergy.com>
2021-12-26 20:37:28 +0100

    Migrate libsodium tests to use uktest
    Migrate libsodium tests to use uktest and introduce the libsodium_minimal
    testuite.
    
    Tests are enabled either by selecting either of the CONFIG_LIBSODIUM_TESTS
    or CONFIG_LIBUKTEST_ALL options.
    
    Resolves #2
    
    Signed-off-by: Michalis Pappas <Michalis.Pappas@opensynergy.com>

--- include/sodium/uk_sodium_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SODIUM_UK_TEST_H__
-#define __SODIUM_UK_TEST_H__
-
-/* Run libsodium's tests.
- * @return number of failed tests, or zero if all passed.
- */
-int uk_sodium_run_tests(void);
-
-#endif /* __SODIUM_UK_TEST_H__ */
